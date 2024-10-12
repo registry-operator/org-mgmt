b755ade0b519e53fc17578b7a39617946e8ce47e
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-10-12 17:50:00 +0200

    chore: add templates
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.tf
+++ repositories.tf
@@ -30,6 +30,13 @@ module "org_mgmt" {
     "registry-operator",
   ]
 
+  labels = [
+    { name = "area/dependency", color = "0052cc", description = "Issues or PRs related to dependency changes." },
+    { name = "do-not-merge", color = "e11d21", description = "Indicates that a PR should not merge." },
+    { name = "kind/security", color = "e11d21", description = "Categorizes issue or PR as related to a security." },
+    { name = "kind/membership", color = "c7def8", description = "Categorizes issue or PR as related to a new feature." },
+  ]
+
   required_status_checks = [
     "DCO",
     "pr-title",
