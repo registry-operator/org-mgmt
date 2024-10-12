73d35d901795eec7e5dc4cce48927c18788e05a2
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-10-12 18:00:35 +0200

    feat: add new milestone
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- repositories.tf
+++ repositories.tf
@@ -1,7 +1,7 @@
 module "registry_operator" {
-  source = "./modules/repository"
-
+  source       = "./modules/repository"
   name         = "registry-operator"
+  description  = "Operator for CNCF Distribution Registry 📦"
   archived     = false
   is_public    = true
   homepage_url = "registry-operator.dev"
@@ -20,21 +20,19 @@ module "registry_operator" {
 }
 
 module "org_mgmt" {
-  source = "./modules/repository"
-
-  name      = "org-mgmt"
-  archived  = false
-  is_public = true
-  topics = [
-    "opentofu",
-    "registry-operator",
-  ]
+  source      = "./modules/repository"
+  name        = "org-mgmt"
+  description = "Management of org via OpenTofu and GitHub Actions"
+  archived    = false
+  is_public   = true
+  topics      = ["opentofu", "registry-operator"]
 
   labels = [
     { name = "area/dependency", color = "0052cc", description = "Issues or PRs related to dependency changes." },
     { name = "do-not-merge", color = "e11d21", description = "Indicates that a PR should not merge." },
-    { name = "kind/security", color = "e11d21", description = "Categorizes issue or PR as related to a security." },
     { name = "kind/membership", color = "c7def8", description = "Categorizes issue or PR as related to a new feature." },
+    { name = "kind/security", color = "e11d21", description = "Categorizes issue or PR as related to a security." },
+    { name = "kind/support", color = "c7def8", description = "Categorizes issue or PR as support-related." },
   ]
 
   required_status_checks = [
